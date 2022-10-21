b29dc5c6bbb697b7f324a06fdfc988bf85c41020
shtorm901 <tetenkov.com@mail.ru>
2022-10-21 16:32:57 +0300

    была обновлена база данных

--- museum.py
+++ museum.py
@@ -8,6 +8,7 @@ table_workers = """CREATE TABLE IF NOT EXISTS workers(
                 named VARCHAR(20),
                 middle_name VARCHAR(30),
                 telephone_number VARCHAR(30),
+                UNIQUE (surname, named, middle_name, telephone_number),
                 FOREIGN KEY (Post_id) REFERENCES Post(Post_id)
 
 )"""
@@ -15,7 +16,8 @@ table_hall = """CREATE TABLE IF NOT EXISTS hall(
             Hall_id INTEGER PRIMARY KEY AUTOINCREMENT,
             Title_Hall VARCHAR(30),
             Floor INTEGER,
-            Square INTEGER
+            Square INTEGER,
+            UNIQUE (Title_Hall)
 
 )"""
 
@@ -24,6 +26,7 @@ table_exhibition_exhibits = """CREATE TABLE IF NOT EXISTS exhibition_exhibits(
                             Hall_id INTEGER,
                             Title_Exhibit VARCHAR(30),
                             Date_of_discovery DATA,
+                            UNIQUE(Title_Exhibit),
                             FOREIGN KEY (Hall_id) REFERENCES Hall(Hall_id)
 
 )"""
@@ -35,19 +38,22 @@ table_excursion = """CREATE TABLE IF NOT EXISTS excursion(
                     Exhibit_id INTEGER,
                     Workers_id INTEGER,
                     Number_of_People INTEGER,
+                    UNIQUE(Exhibit_id),
                     FOREIGN KEY (Exhibit_id) REFERENCES Exhibit(Exhibit_id),
                     FOREIGN KEY (Workers_id) REFERENCES Workers(Workers_id)
 )"""
 
 table_post = """CREATE TABLE IF NOT EXISTS post(
             Post_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Post VARCHAR(30)
+            Post VARCHAR(30),
+            UNIQUE(Post_id, Post)
 
 )"""
 
 table_User = """CREATE TABLE IF NOT EXISTS users(
             User_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            User_Name VARCHAR(30)
+            User_Name VARCHAR(30),
+            UNIQUE(User_id, User_name)
 
 )"""
 
@@ -55,7 +61,6 @@ with connection:
     try:
         cursor = connection.cursor()
         cursor.execute(table_workers)
-        cursor.execute("DELETE FROM workers")
         cursor.execute('INSERT INTO workers(Post_id, named, surname, middle_name, telephone_number) VALUES ("2","Aleksei", "Kamarov", "Ivanovich", "None")')
         connection.commit()
         cursor.execute('INSERT INTO workers(Post_id, named, surname, middle_name, telephone_number) VALUES ("1","Michail", "Morozov", "Alekseevich", "+7(999(617-19-02")')
@@ -65,7 +70,6 @@ with connection:
         cursor.execute('INSERT INTO workers(Post_id, named, surname, middle_name, telephone_number) VALUES ("3", "Anna", "Leonteva", "Andreevna", "+7(937)225-02-37")')
         connection.commit()
         cursor.execute(table_post)
-        cursor.execute('DELETE FROM Post')
         cursor.execute('INSERT INTO post(Post) VALUES ("Уборщик")')
         connection.commit()
         cursor.execute('INSERT INTO post(Post) VALUES ("Охранник")')
@@ -75,7 +79,6 @@ with connection:
         cursor.execute('INSERT INTO post(Post) VALUES ("Заведующий музеем")')
         connection.commit()
         cursor.execute(table_hall)
-        cursor.execute('DELETE FROM hall')
         cursor.execute('INSERT INTO hall(Title_Hall, Floor, Square) VALUES ("бытовые предметы времен второй мировой войны", "1", "1")')
         connection.commit()
         cursor.execute('INSERT INTO hall(Title_Hall, Floor, Square) VALUES ("оружие времен второй мировой войны", "2", "3")')
